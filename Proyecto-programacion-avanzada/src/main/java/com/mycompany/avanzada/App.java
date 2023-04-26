9ddcf786f5c7d4ada0165a96fe5c013c904b4e20
Sashiiii <80544058+Sashiiii@users.noreply.github.com>
2023-04-26 12:58:49 -0400

    Add files via upload

--- Proyecto-programacion-avanzada/src/main/java/com/mycompany/avanzada/App.java
+++ Proyecto-programacion-avanzada/src/main/java/com/mycompany/avanzada/App.java
@@ -11,22 +11,22 @@ import java.util.Scanner;
 public class App {
    public static void main(String arg[]) throws IOException {
     int opcion; //variable para que el usuario escoja en el menu
-    int indice; //variable para almacenar el indice que retorna el metodo que compreuba si un producto ya esta en la lista
+    int indice; //variable para almacenar el indice que retorna el metodo que comprueba si un producto ya esta en la lista
     String a, linea; // a: almacena lo que el usuario ingrese antes de transformarlo a int, linea almacena las lineas leidas del archivo .txt
     String[] actual; //almacena los parametros que contiene una linea, pero por separado
     File f = new File("./productos.txt"); //se abre el archivo que contiene la lista de productos
     Scanner s = new Scanner(f);//se inicializa un scanner para trabajar con el archivo
     BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));//se inicializa lector para leer input
-    Map<String,tipoSeccion> bodega = new HashMap<String, tipoSeccion>(); //se crea mapa para acceder mas facilmente al stock
-    
-    while(s.hasNext()){ //mientras queden lineas sin leer se agregaran los contenidos de cada una en el mapa usando como key la seccion
+    Map<String,TipoSeccion> bodega = new HashMap<String, TipoSeccion>(); //se crea mapa para acceder mas facilmente al stock
+    //mientras queden lineas sin leer se agregaran los contenidos de cada una en el mapa usando como key la seccion
+    while(s.hasNext()){ 
          linea = s.nextLine();
          actual = linea.split(",");
          if (bodega.containsKey(actual[4])) { //se comprueba que esta seccion ya se encuentre en el mapa
         	 bodega.get(actual[4]).agregarProducto(actual);
          }
          else { //si no se encuentra se crea la seccion
-        	 tipoSeccion newS = new tipoSeccion();
+        	 TipoSeccion newS = new TipoSeccion();
         	 newS.setNomSeccion(actual[4]);
         	 newS.agregarProducto(actual);
         	 bodega.put(actual[4], newS);
@@ -42,44 +42,46 @@ public class App {
             a = lector.readLine();
             opcion = Integer.parseInt(a);
             
-            if(opcion==1) {
-            	for(Entry<String, tipoSeccion> r : bodega.entrySet()) {
-            		System.out.println("\nSeccion: "+r.getValue().getNomSeccion());
-            		r.getValue().enlistarProductos();
-            	}
-            }else if(opcion==2){
-                actual = new String[5];
+            switch(opcion) {
+            	case 1:
+            		for(Entry<String, TipoSeccion> r : bodega.entrySet()) {
+            			System.out.println("\nSeccion: "+r.getValue().getNomSeccion());
+            			r.getValue().enlistarProductos();
+            		}
+            		break;
+            	case 2:
+            		actual = new String[5];
                 
-                System.out.print("Ingrese seccion del producto: ");
-                actual[4] = lector.readLine();
-                if (!bodega.containsKey(actual[4])) { //se comprueba que esta seccion ya se encuentre en el mapa
-                    tipoSeccion newS = new tipoSeccion();
-                    newS.setNomSeccion(actual[4]);
-                    bodega.put(actual[4], newS);
-                }
+            		System.out.print("Ingrese seccion del producto: ");
+            		actual[4] = lector.readLine();
+            		//se comprueba que esta seccion ya se encuentre en el mapa
+            		if (!bodega.containsKey(actual[4])) { 
+            			TipoSeccion newS = new TipoSeccion();
+            			newS.setNomSeccion(actual[4]);
+            			bodega.put(actual[4], newS);
+            		}
                
-                System.out.print("Ingrese nombre del producto: ");
-                actual[0] = lector.readLine();
-                indice=bodega.get(actual[4]).esta(actual[0]);
-                if(indice>=0) {
-                	
-                	bodega.get(actual[4]).agregarStock(indice);
-                	continue;
-                }
+            		System.out.print("Ingrese nombre del producto: ");
+            		actual[0] = lector.readLine();
+            		indice = bodega.get(actual[4]).esta(actual[0]);
+            		if(indice >= 0) {
+            			bodega.get(actual[4]).agregarStock(indice);
+            			continue;
+            		}
                
-               System.out.print("Ingrese codigo del producto: ");
-                actual[1] = lector.readLine();
-                System.out.print("Ingrese stock del producto: ");
-                actual[2] = lector.readLine();
-                System.out.print("Ingrese precio del producto: ");
-                actual[3] = lector.readLine();
+            		System.out.print("Ingrese codigo del producto: ");
+            		actual[1] = lector.readLine();
+            		System.out.print("Ingrese stock del producto: ");
+            		actual[2] = lector.readLine();
+            		System.out.print("Ingrese precio del producto: ");
+            		actual[3] = lector.readLine();
                 
-                bodega.get(actual[4]).agregarProducto(actual);
-                
+            		bodega.get(actual[4]).agregarProducto(actual);
+            		break;
             }
 	}while(opcion!=0);
         //se cierra archivo
-	s.close();
+		s.close();
     }
 
 
